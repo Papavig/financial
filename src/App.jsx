039a1bbdc19fac6dbcdb5d72df6cfff9f145fb12
Papavig <beingvig@gmail.com>
2023-12-13 09:44:04 +0530

    Dashboard and backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
+import Error from "./components/Error";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <>
       <div className="text-center bg-main">
-        {/* <Navbar /> */}
-        <div className="sm:container sm:mx-auto">
-          <Home />
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/404" element={<Error />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+          </Router>
         </div>
-      </div>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { TiTick } from "react-icons/ti";
 import { BsCurrencyRupee } from "react-icons/bs";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const signup = () => {
+    navigate("/signup");
+  };
+
+  const error = () => {
+    navigate("/404");
+  };
+
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
@@ -17,13 +28,16 @@ function Home() {
           <h1>Master your financial universe</h1>
         </div>
         <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mt-8 mb-20">
-          <button className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-4 rounded focus:outline-none">
+          <button
+            className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-4 rounded focus:outline-none"
+            onClick={signup}
+          >
             Get started
           </button>
         </div>
       </div>
 
-      <div className="container mx-auto my-12 mx-6 p-12 rounded-lg shadow-lg grid grid-cols-1 md:grid-cols-2 gap-8 h-auto md:h-screen">
+      <div className="container my-12 mx-6 p-12 rounded-lg shadow-lg grid grid-cols-1 md:grid-cols-2 gap-8 h-auto md:h-screen sm:container sm:mx-auto">
         <div className="md:order-2 text">
           <img
             src="/assets/building1.png"
@@ -31,11 +45,11 @@ function Home() {
             className="object-cover rounded-md w-full h-64 sm:w-2/3 sm:h-auto md:w-1/2 md:h-full lg:w-2/3 lg:h-2/3 xl:w-2/3 xl:h-2/3 mx-auto mb-4 md:mb-0"
           />
         </div>
-        <div className="md:order-1 text-left">
+        <div className="md:order-1 text-left md:ml-10 md:pl-12">
           <h2 className="text-4xl sm:text-2xl md:text-4xl text-mainText font-pixel font-bold mb-2 md:mb-4">
             Chart Your Financial Course: Empower Your Wealth
           </h2>
-          <p className="text-white mb-4 text-base sm:text-lg md:text-base lg:text-lg">
+          <p className="text-white mb-4 text-base sm:text-lg md:text-base lg:text-lg opacity-70">
             Your financial journey is like sailing through uncharted waters.
             With each decision, you hold the compass to "Chart Your Financial
             Course: Empower Your Wealth." It's not just about the destination;
@@ -54,11 +68,11 @@ function Home() {
             className="object-cover rounded-md w-full h-64 sm:w-2/3 sm:h-auto md:w-1/2 md:h-full lg:w-2/3 lg:h-2/3 xl:w-2/3 xl:h-2/3 mx-auto mb-4 md:mb-0"
           />
         </div>
-        <div className="md:order-4 text-left">
+        <div className="md:order-4 text-left md:mr-10 md:pr-12">
           <h2 className="text-4xl sm:text-2xl md:text-4xl text-mainText font-pixel font-bold mb-2 md:mb-4">
             Compose Your Financial Symphony: Craft Success
           </h2>
-          <p className="text-white mb-4 text-base sm:text-lg md:text-base lg:text-lg">
+          <p className="text-white mb-4 text-base sm:text-lg md:text-base lg:text-lg opacity-70">
             Life is a grand composition, and your financial decisions are the
             notes that shape it. "Compose Your Financial Symphony: Craft
             Success" invites you to be the conductor of your financial destiny.
@@ -72,16 +86,19 @@ function Home() {
         </div>
       </div>
 
-      <div className="container px-4 md:px-8 lg:px-16 xl:px-32 pb-8 pt-12 md:pt-16 lg:pt-20">
+      <div className="container px-4 md:px-8 lg:px-16 xl:px-32 pb-8 pt-12 md:pt-16 lg:pt-20 sm:container sm:mx-auto">
+        <h3 className="text-white text-5xl font-bold font-pixel my-12 opacity-80">
+          Plans
+        </h3>
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           <div className="shadow-lg rounded-3xl py-8 px-6 bg-mainAccent">
             <span className="mb-3 inline-block text-lg font-semibold text-white">
               Basic
             </span>
             <h3 className="font-pixel font-bold text-mainText text-4xl sm:text-6xl mb-6 flex items-center">
-              <BsCurrencyRupee className="text-2xl" /> 299
+              free
             </h3>
-            <ul className="space-y-3">
+            <ul className="space-y-3 opacity-70">
               <li className="text-sm text-white flex items-center">
                 <TiTick />
                 Budget Tracker
@@ -99,7 +116,10 @@ function Home() {
                 Financial Reports
               </li>
             </ul>
-            <button className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-6 w-full rounded focus:outline-none mt-6">
+            <button
+              className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-6 w-full rounded focus:outline-none mt-6"
+              onClick={signup}
+            >
               Sign Up
             </button>
           </div>
@@ -109,9 +129,9 @@ function Home() {
               Premium
             </span>
             <h3 className="font-pixel font-bold text-mainText text-4xl sm:text-6xl mb-6 flex items-center">
-              <BsCurrencyRupee className="text-2xl" /> 399
+              <BsCurrencyRupee className="text-2xl" /> 299
             </h3>
-            <ul className="space-y-3">
+            <ul className="space-y-3 opacity-70">
               <li className="text-sm text-white flex items-center">
                 <TiTick />
                 All Basic
@@ -129,8 +149,11 @@ function Home() {
                 Financial Planning
               </li>
             </ul>
-            <button className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-6 w-full rounded focus:outline-none mt-6">
-              Sign Up
+            <button
+              className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-6 w-full rounded focus:outline-none mt-6"
+              onClick={error}
+            >
+              Get Premium
             </button>
           </div>
 
@@ -139,9 +162,9 @@ function Home() {
               Ultimate
             </span>
             <h3 className="font-pixel font-bold text-mainText text-4xl sm:text-6xl mb-6 flex items-center">
-              <BsCurrencyRupee className="text-2xl" /> 499
+              <BsCurrencyRupee className="text-2xl" /> 399
             </h3>
-            <ul className="space-y-3">
+            <ul className="space-y-3 opacity-70">
               <li className="text-sm text-white flex items-center">
                 <TiTick />
                 All Premium
@@ -159,8 +182,11 @@ function Home() {
                 Wealth Management
               </li>
             </ul>
-            <button className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-6 w-full rounded focus:outline-none mt-6">
-              Sign Up
+            <button
+              className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-6 w-full rounded focus:outline-none mt-6"
+              onClick={error}
+            >
+              Get Ultimate
             </button>
           </div>
         </div>
@@ -169,12 +195,15 @@ function Home() {
           <h1 className="text-2xl sm:text-3xl lg:text-4xl text-mainText font-pixel mb-4">
             Ready, Set
           </h1>
-          <p className="text-sm sm:text-base lg:text-lg text-white leading-relaxed mb-8">
+          <p className="text-sm sm:text-base lg:text-lg text-white leading-relaxed mb-8 opacity-70">
             Ready to get a head start on your financial future?
             <br />
             Sign up, it's time to watch your wealth grow!
           </p>
-          <button className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-4 sm:py-3 sm:px-6 rounded focus:outline-none">
+          <button
+            className="bg-btn1 hover:bg-hbtn1 text-white font-medium py-2 px-4 rounded focus:outline-none"
+            onClick={signup}
+          >
             Start Now
           </button>
         </div>
